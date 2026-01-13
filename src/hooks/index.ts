974404d3df2218e08d4e432651a98cc1ec509fbf
Rapemo <allenrapemo@gmail.com>
2026-01-13 21:54:26 +0300

    Fix hooks import resolution error
    - Create src/hooks/index.ts to export all hooks
    - Fix Vite import resolution for @/hooks path
    - Enable proper module bundling for hooks usage

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+// Re-export all hooks from this directory
+export { useCompanies } from './useCompanies';
+export { useCustomers } from './useCustomers';
+export { useDeals } from './useDeals';
+export { useQuotes } from './useQuotes';
+export { useProviders } from './useProviders';
+export { useActivities } from './useActivities';
+export { useDashboard } from './useDashboard';
